ed4a5a9603ac62fa8cc23bcba5fd4cdc0df03289
Git1990User <90474562+Git1990User@users.noreply.github.com>
2021-09-11 00:05:13 +0500

    Adding test project

---
+++ src/test/java/com/login/LoginScenarios.java
@@ -0,0 +1,36 @@
+package com.login;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.AssertJUnit;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.Status;
+import com.extent.report.ExtentReportBaseClass;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginScenarios extends ExtentReportBaseClass  {
+
+	
+	public static ChromeDriver driver;	 
+	
+	
+	@Test
+    public void GoogleTest() {
+	test = extent.createTest("Testing integration", "Testing integration"); 
+	WebDriverManager.chromedriver().setup();
+	driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.google.com/");
+	driver.findElementByName("q").sendKeys("Mobile phones");
+	driver.close();
+	
+    }
+	
+	
+	
+
+}
