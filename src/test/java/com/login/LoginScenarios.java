d3cfe5cf54cd8197971628aeb8ccefc597281c3f
Git1990User <iqra.andleeb01@gmail.com>
2021-09-11 14:58:56 +0500

    updating file

---
+++ src/test/java/com/login/LoginScenarios.java
@@ -0,0 +1,40 @@
+package com.login;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.AssertJUnit;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.Status;
+import com.extent.report.ExtentReportBaseClass;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginScenarios extends ExtentReportBaseClass  {
+
+	
+	public static ChromeDriver driver;	 
+	
+	
+	@Test
+    public void GoogleTest() {
+	test = extent.createTest("Testing integration", "Testing integration"); 
+	System.out.println("hello world");
+	WebDriverManager.chromiumdriver().setup();
+	driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.google.com/");
+	driver.findElementByName("q").sendKeys("Mobile phones");
+	driver.close();
+	
+	
+    }
+	
+	
+	
+
+}
